41e6288b16117e6dbdee96b946d5528a31551963
Cody Russell <cody@jhu.edu>
2014-05-27 02:54:24 -0500

    Changes

--- main.m
+++ main.m
@@ -15,21 +15,20 @@ main (int argc, char *argv[])
 
   gtk_init (&argc, &argv);
 
+  // Create top-level GtkWindow
   window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
   gtk_widget_show (window);
   gdkwindow = gtk_widget_get_window (window);
-  nsview = gdk_quartz_window_get_nsview (gdkwindow);
   nswindow = gdk_quartz_window_get_nswindow (gdkwindow);
 
   NSBox *box = [[NSBox alloc] initWithFrame:nswindow.frame];
   NSButton *nsbutton = [[NSButton alloc] initWithFrame:box.frame];
   NSGtkView *foo = [[NSGtkView alloc] initWithFrame:box.frame];
-  //[box addSubview:nsbutton];
+  [box addSubview:foo];
   [foo setNeedsDisplay:YES];
 
-  gtknsview = gtk_ns_view_new (foo);
-  gtk_widget_show_all (gtknsview);
-
+  // Create a GtkNSView. This is our NSView top-level
+  gtknsview = gtk_ns_view_new (box);
 
   GtkWidget *bar = gtk_button_new_with_label ("gtk button");
 
@@ -38,15 +37,10 @@ main (int argc, char *argv[])
   [foo setGtkWidget:bar];
   gtk_embed_container_set_gtknsview (GTK_EMBED_CONTAINER (embed),
                                      GTK_NS_VIEW (gtknsview));
-  gtk_widget_show_all (embed);
 
-
-  // GtkWidget *embed = gtk_hbox_new (FALSE, 0);
-  // gtk_container_add (GTK_CONTAINER (embed), gtknsview);
-  // gtk_widget_show_all (embed);
-
-
+  // Add the embed container to the window
   gtk_container_add (GTK_CONTAINER (window), embed);
+  gtk_widget_show_all (window);
 
   gtk_main ();
 
